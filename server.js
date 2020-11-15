f7a571664bf481ff7f5758abc88d0887ecffc3cb
vraj <vraj@vishwarajbhattrai.local>
2020-11-15 15:38:43 +0530

    add user mode and connect to the db

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ mongoose.connect('mongodb://localhost/userData')
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+ User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
 })
 
 app.route('/users/:id')
